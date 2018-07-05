2af5c989e66e18f04e298cd41aa7078a173d208f
Lucas Gabriel Coelho Vieira <vieraf1@hotmail.com.br>
2018-07-05 15:31:41 -0300

    Add files via upload

---
+++ APA3.cpp
@@ -0,0 +1,30 @@
+#include "iostream"
+#include "cstdlib"
+#include "math.h"
+using namespace std;
+
+int main() {
+    float no1, no2, media;
+    
+    system("cls");
+    cout << "Programa media: \n" << endl;
+    cout << "Digite nota 1: "; cin >> no1;
+    cout << "\nDigite nota 2: "; cin >> no2;
+    
+    media = (no1 + no2)/2;
+    
+    cout << "\nMedia: " << media << endl << endl;
+    
+    system("pause");
+    system("cls");
+    
+    if(media < 6){
+        cout << "\nReprovado...\n\n";
+             }
+    else {
+        cout << "\nAprovado!!!\n\n";
+    }
+    system("pause");
+    
+    return 0; }
+    
